6c103dbed5d861a4727014643f8e4a4d8902d2ad
cristianbyte <charlsmaritz@gmail.com>
2023-04-23 22:10:41 -0500

    first commit again

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Menu from './component/menu'
+
+function App() {
+
+  return (
+    <>
+      <Menu/>
+    </>
+  )
+}
+
+export default App
---
+++ src/component/menu.jsx
@@ -0,0 +1,19 @@
+import './menu.css'
+
+export default function Menu() {
+
+  return (
+    <div className="menu">
+        <div className="menu__logo">
+            cristianbyte
+        </div>
+        <div className="menu__option">
+            <div className="menu__option--i">Home</div>
+            <div className="menu__option--i">Proyects</div>
+            <div className="menu__option--i">About Me</div>
+            <div className="menu__option--i">Contact</div>
+        </div>
+    </div>
+  )
+}
+
